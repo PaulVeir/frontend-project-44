92dd2431955342f302d969e77587079825736d36
Pavel Mushkaterov <pavel.mush@gmail.com>
2023-09-20 23:30:54 +0300

    Refactoring even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,25 +1,10 @@
-import {
-  getGreeting,
-  isAllCorrect,
-  getQuestions,
-  getRandomize,
-} from '../index.js';
+import { runGame, getGreeting, getInstructions } from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
 
-const getInstructions = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
 export default () => {
   const name = getGreeting();
-  getInstructions();
-  for (let i = 0; i < 3; i += 1) {
-    const num = getRandomize();
-    const result = isEven(num) ? 'yes' : 'no';
-    const message = getQuestions(num, result, name);
-    if (!message) {
-      break;
-    } else {
-      isAllCorrect(i, message, name);
-    }
-  }
+  const [evenInstructions] = getInstructions();
+  console.log(evenInstructions);
+  runGame(isEven, name);
 };
