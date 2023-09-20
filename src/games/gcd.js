6cc570964fbb3dc3db3cb375fab4a723137f750c
Pavel Mushkaterov <pavel.mush@gmail.com>
2023-09-20 23:31:28 +0300

    Refactoring gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,15 +3,15 @@ import {
   isAllCorrect,
   getQuestions,
   getRandomize,
+  getInstructions,
 } from '../index.js';
 
 const getGcd = (a, b) => ((a % b) ? getGcd(b, a % b) : Math.abs(b));
 
-const getInstructions = () => console.log('Find the greatest common divisor of given numbers.');
-
 export default () => {
   const name = getGreeting();
-  getInstructions();
+  const [,, gcdInstructions] = getInstructions();
+  console.log(gcdInstructions);
   for (let i = 0; i < 3; i += 1) {
     const num1 = getRandomize();
     const num2 = getRandomize();
