fbb7c7c483691ff844a9b78d0e3bbd39e8980b68
Pavel Mushkaterov <pavel.mush@gmail.com>
2023-09-24 20:26:26 +0300

    Refactoring gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,27 +1,20 @@
 import {
-  getGreeting,
-  isAllCorrect,
-  getQuestions,
+  runGames,
   getRandomize,
   getInstructions,
 } from '../index.js';
 
 const getGcd = (a, b) => ((a % b) ? getGcd(b, a % b) : Math.abs(b));
 
-export default () => {
-  const name = getGreeting();
+const runGcdGame = () => {
   const [,, gcdInstructions] = getInstructions();
-  console.log(gcdInstructions);
-  for (let i = 0; i < 3; i += 1) {
-    const num1 = getRandomize();
-    const num2 = getRandomize();
-    const expression = `${num1} ${num2}`;
-    const result = getGcd(num1, num2).toString();
-    const message = getQuestions(expression, result, name);
-    if (!message) {
-      break;
-    } else {
-      isAllCorrect(i, message, name);
-    }
-  }
+  const num1 = getRandomize();
+  const num2 = getRandomize();
+  const expression = `${num1} ${num2}`;
+  const result = getGcd(num1, num2).toString();
+  return [expression, result, gcdInstructions];
+};
+
+export default () => {
+  runGames(runGcdGame);
 };
