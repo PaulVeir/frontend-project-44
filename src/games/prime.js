9bb6bd325b82c9693b53bca3e68acb2dfb6b0858
Pavel Mushkaterov <pavel.mush@gmail.com>
2023-11-08 18:33:16 +0300

    Refactoring prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,24 +1,22 @@
 import {
   runGames,
-  getInstructions,
   getRandomize,
 } from '../index.js';
 
 const isPrime = (num) => {
-  let result = true;
   if (num === 1) {
-    result = false;
+    return false;
   }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      result = false;
+      return false;
     }
   }
-  return result;
+  return true;
 };
 
 const runPrimeGame = () => {
-  const [,,,, primeInstructions] = getInstructions();
+  const primeInstructions = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const num = getRandomize();
   const result = isPrime(num) ? 'yes' : 'no';
   return [num, result, primeInstructions];
